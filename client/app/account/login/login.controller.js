f27779bfbf5a6bc0c71c2a05a615551d7fc29590
Justin Lawson <jaglawson3@gmail.com>
2016-03-21 12:03:22 -0600

    beginning to work on nested views for story

--- client/app/account/login/login.controller.js
+++ client/app/account/login/login.controller.js
@@ -20,7 +20,7 @@ class LoginController {
       })
       .then(() => {
         // Logged in, redirect to home
-        this.$state.go('main');
+        this.$state.go('Story');
       })
       .catch(err => {
         this.errors.other = err.message;
--- client/app/app.js
+++ client/app/app.js
@@ -11,9 +11,32 @@ angular.module('theFableHouseApp', [
   'ui.bootstrap',
   'validation.match'
 ])
-  .config(function($urlRouterProvider, $locationProvider) {
+  .config(function($urlRouterProvider, $locationProvider, $stateProvider) {
     $urlRouterProvider
       .otherwise('/');
 
     $locationProvider.html5Mode(true);
+
+    $stateProvider
+    .state('index', {
+      url: "",
+      views: {
+        "viewA": { template: "index.viewA" },
+        "viewB": { template: "index.viewB" }
+      }
+    })
+    .state('route1', {
+      url: "/route1",
+      views: {
+        "viewA": { template: "route1.viewA" },
+        "viewB": { template: "route1.viewB" }
+      }
+    })
+    .state('route2', {
+      url: "/route2",
+      views: {
+        "viewA": { template: "route2.viewA" },
+        "viewB": { template: "route2.viewB" }
+      }
+    })
   });
