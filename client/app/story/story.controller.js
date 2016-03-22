4ac7b4335243412d45a37a33585dcf2f446f9287
Justin Lawson <jaglawson3@gmail.com>
2016-03-22 10:28:37 -0600

    Decreased video size from 50MB to 1.4MB with handbrake. Now on to implementing the story functionality with ng bind.

--- client/app/story/story.controller.js
+++ client/app/story/story.controller.js
@@ -5,7 +5,8 @@
     constructor($scope, $http) {
       this.$http = $http;
       this.$scope = $scope;
-      this.$scope.message = 'Hello';
+      this.$scope.video1 = '../assets/images/chapter1Modified.mp4';
+      // this.$scope.video2 = '../assets/images/chapter2.mp4';
       this.$http.get('../assets/images/story_tree.json').then(response => {
         console.log(response);
         this.$scope.storyPath = response.data;
