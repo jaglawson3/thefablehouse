dbe8e26489b5e15cd6511883fcd8af673a298f0d
Justin Lawson <jaglawson3@gmail.com>
2016-03-23 14:31:54 -0600

    modified some of the styling in the story view

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,10 +2,15 @@
 
 (function() {
 
-class MainController {
-  constructor($http) {
+class MainController {constructor($http) {
     this.$http = $http;
     this.awesomeThings = [];
+  }
+
+  $onInit() {
+    this.$http.get('/api/things').then(response => {
+      this.awesomeThings = response.data;
+    });
   }
 
   addThing() {
@@ -18,23 +23,12 @@ class MainController {
   deleteThing(thing) {
     this.$http.delete('/api/things/' + thing._id);
   }
-
 }
 
 angular.module('theFableHouseApp')
   .component('main', {
     templateUrl: 'app/main/main.html',
     controller: MainController
-  })
-  .directive('video', function () {
-    return {
-      restrict: 'E',
-      link: function(scope, element) {
-        scope.$on('$destroy', function() {
-          element.prop('src', '');
-        });
-      }
-    };
   });
 
 })();
